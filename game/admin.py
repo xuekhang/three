50c474112c2a96c288f32d2e0109e756b9f1c1e3
Xue <xuekhang91@gmail.com>
2021-02-12 17:55:28 -0600

    no message

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Config, Game, Player
 
 # Register your models here.
+admin.site.register(Config)
+admin.site.register(Game)
+admin.site.register(Player)
