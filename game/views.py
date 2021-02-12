7344bbd9da64c7a1d19cbb4e4c1ebeabca375835
Xue <xuekhang91@gmail.com>
2021-02-11 19:29:03 -0600

    semi working form

--- game/views.py
+++ game/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ConfigForm
 import random
 import logging
 
 # Create your views here.
 def home(request):
-    context = {'title':'home'}
+    
     myList = [1,2]
+
+    context = {
+        'title':'home'}   
 
     if request.method == 'POST':
         return render(request, 'game/config.html', context)
+    else:
+        form = ConfigForm()
 
+    context = {
+        'title':'home',
+        'form': form}    
     return render(request, 'game/home.html', context)
 
 def config(request):
