67397ecca15ef6f115dec20194cb612f83151dcf
Xue <xuekhang91@gmail.com>
2021-02-12 19:08:10 -0600

    no message

--- game/models.py
+++ game/models.py
@@ -5,15 +5,21 @@ from multiselectfield import MultiSelectField
 
 class Game(models.Model):
     code = models.CharField(max_length=6)
+    def __str__(self):
+        return self.code
 
 class Config(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     num_of_player = models.IntegerField()
     letter = models.CharField(max_length=1)
+    def __str__(self):
+        return self.game.code + '   ' + str(self.num_of_player)
 
 class Player(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     BOOK_CHOICES = (
