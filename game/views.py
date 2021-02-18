d8005c965eee6451002a8ed08a9f4faa6ed37318
Xue <xuekhang91@gmail.com>
2021-02-18 17:25:15 -0600

    no message

--- game/views.py
+++ game/views.py
@@ -198,7 +198,7 @@ def board(request, game_code='', player_name='', round_num=''):
             for category in categories_in_round:
                 categories.append(category.name)
             player = Player.objects.get(game=game,name=player_name)
-            test = 12331231321
+            test = 1233123521
 
             context = {
                 'title': 'board',
