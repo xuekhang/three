af534e3d73aff184263822257ca2b3845d9b081a
Xue <xuekhang91@gmail.com>
2021-02-21 17:28:24 -0600

    no message

--- game/admin.py
+++ game/admin.py
@@ -11,14 +11,52 @@ from .models import (
     Answer
 )
 
-# Register your models here.
-admin.site.register(Config)
-admin.site.register(Game)
-admin.site.register(Player)
 
-admin.site.register(GlobalCategory)
-admin.site.register(LocalCategory)
-admin.site.register(Round)
-admin.site.register(CategoryInRound)
-admin.site.register(Question)
-admin.site.register(Answer)
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Config._meta.get_fields()]
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Game._meta.get_fields()]
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    # list_display = [field.name for field in Player._meta.get_fields()]
+    list_display = ['name','game','is_host']
+
+
+class GlobalCategoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in GlobalCategory._meta.get_fields()]
+
+
+class LocalCategoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in LocalCategory._meta.get_fields()]
+
+
+class RoundAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Round._meta.get_fields()]
+
+
+class CategoryInRoundAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CategoryInRound._meta.get_fields()]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Question._meta.get_fields()]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Answer._meta.get_fields()]
+
+
+# Register your models here.
+admin.site.register(Config, ConfigAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Player, PlayerAdmin)
+
+admin.site.register(GlobalCategory, GlobalCategoryAdmin)
+admin.site.register(LocalCategory, LocalCategoryAdmin)
+admin.site.register(Round, RoundAdmin)
+admin.site.register(CategoryInRound, CategoryInRoundAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
