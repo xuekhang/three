63c4f006ceac357e9a2bfbe397dd45d1b16ee6a2
Xue <xuekhang91@gmail.com>
2021-02-21 19:33:52 -0600

    no message

--- game/models.py
+++ game/models.py
@@ -7,8 +7,8 @@ from multiselectfield import MultiSelectField
 class Game(models.Model):
     code = models.CharField(max_length=6)
 
-    # def __str__(self):
-    #     return self.code
+    def __str__(self):
+        return self.code
 
 
 class Config(models.Model):
@@ -27,8 +27,8 @@ class Config(models.Model):
     num_of_cat_per_round = models.IntegerField()
     letters = MultiSelectField(choices=letter_choices)
 
-    # def __str__(self):
-    #     return self.game.code + '   ' + str(self.num_of_players)
+    def __str__(self):
+        return self.game.code + '   ' + str(self.num_of_players)
 
 
 class Player(models.Model):
@@ -36,23 +36,23 @@ class Player(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     is_host = models.BooleanField()
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
 
 
 class GlobalCategory(models.Model):
     name = models.CharField(max_length=100)
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
 
 
 class LocalCategory(models.Model):
     game = models.OneToOneField(Game, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
 
 
 class Round(models.Model):
@@ -61,16 +61,16 @@ class Round(models.Model):
     letter = models.CharField(max_length=3)
     is_played = models.BooleanField(default=False)
 
-    # def __str__(self):
-    #     return str(self.game) + ' Round: ' + str(self.number)
+    def __str__(self):
+        return str(self.game) + ' Round: ' + str(self.number)
 
 
 class CategoryInRound(models.Model):
     name = models.CharField(max_length=100)
     round = models.ForeignKey(Round, on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     return str(self.round) + ' Cat: ' + self.name
+    def __str__(self):
+        return str(self.round) + ' Cat: ' + self.name
 
 
 class Question(models.Model):
@@ -79,14 +79,14 @@ class Question(models.Model):
     number = models.IntegerField()
     # answer = models.CharField(max_length=150)
 
-    # def __str__(self):
-    #     # + ' Answer: ' + self.answer
-    #     return str(self.round) + ' Player: ' + str(self.player) + ' Category Num: ' + str(self.number)
+    def __str__(self):
+        # + ' Answer: ' + self.answer
+        return str(self.round) + ' Player: ' + str(self.player) + ' Category Num: ' + str(self.number)
 
 
 class Answer(models.Model):
     answer = models.CharField(max_length=150)
     question = models.OneToOneField(Question, on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     return str(self.question) + ' Ans: ' + self.answer
+    def __str__(self):
+        return str(self.question) + ' Ans: ' + self.answer
