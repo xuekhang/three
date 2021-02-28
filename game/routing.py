02d83ec9c1c4ed22c2fb718f99d7bcaa63c072d7
Xue <xuekhang91@gmail.com>
2021-02-28 14:23:41 -0600

    no message

--- game/routing.py
+++ game/routing.py
@@ -6,6 +6,6 @@ from .consumers import (TestConsumer, LobbyConsumer, BoardConsumer)
 ws_urlpatterns = [
     path('', TestConsumer.as_asgi()),
     path('lobby/<game_code>/<player_name>/', LobbyConsumer.as_asgi()),
-    path('board/<game_code>/<player_name>/<round_num>',
+    path('board/<game_code>/<player_name>/<round_num>/',
          BoardConsumer.as_asgi()),
 ]
