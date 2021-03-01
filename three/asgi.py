a14ea91765c42a2799210d5b446e282d285b6581
Xue <xuekhang91@gmail.com>
2021-02-28 19:55:54 -0600

    no message

--- three/asgi.py
+++ three/asgi.py
@@ -6,7 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
+from .wsgi import *
 import os
 
 from django.core.asgi import get_asgi_application
