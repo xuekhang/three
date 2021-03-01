329f1e324786027f63d97aefbacd70a68cdc06fb
Xue <xuekhang91@gmail.com>
2021-02-28 21:53:08 -0600

    no message

--- three/settings.py
+++ three/settings.py
@@ -143,6 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
