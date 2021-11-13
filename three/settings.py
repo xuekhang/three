2036e184127674f2b4ab0fb94f358af4e2610b49
Xue <xuekhang91@gmail.com>
2021-11-12 22:11:09 -0600

    working build

--- three/settings.py
+++ three/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -162,3 +163,4 @@ LOGIN_URL = 'login'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
